845911d51330103ce4fc2e042b7f2e47bc74b6bb
Beatriz Cristiane Soares <91436573+Bbbias@users.noreply.github.com>
2023-10-24 17:22:02 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import Product from './components/product/Product'
+import React, { useEffect, useState } from "react";
+import api from "./services/api";
+
+export default function App() {
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    api
+      .get("/users/bbbias")
+      .then((response) => setUser(response.data))
+      .catch((err) => {
+        console.error("ops! ocorreu um erro" + err);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <p>Usuário: {user?.login}</p>
+      <p>Biografia: {user?.bio}</p>
+    </div>
+  );
+}
